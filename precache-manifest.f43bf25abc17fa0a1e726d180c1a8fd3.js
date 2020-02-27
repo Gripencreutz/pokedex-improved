c3ef0ffb75c4e78273e4a8b8a818a6810f8f5a5a
Greklish <43167188+Greklish@users.noreply.github.com>
2020-02-27 19:12:29 +0100

    Updates

---
+++ precache-manifest.f43bf25abc17fa0a1e726d180c1a8fd3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe5ab34e72371b3643b0cfb9ac5629ba",
+    "url": "/pokedex-improved/index.html"
+  },
+  {
+    "revision": "fbfef9c1e0e18b42d63d",
+    "url": "/pokedex-improved/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "d3c5bea1aa79c18b3c9d",
+    "url": "/pokedex-improved/static/css/main.31a93150.chunk.css"
+  },
+  {
+    "revision": "fbfef9c1e0e18b42d63d",
+    "url": "/pokedex-improved/static/js/2.9bc666cd.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/pokedex-improved/static/js/2.9bc666cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3c5bea1aa79c18b3c9d",
+    "url": "/pokedex-improved/static/js/main.34267526.chunk.js"
+  },
+  {
+    "revision": "df14fd668a4a1d57ea3b",
+    "url": "/pokedex-improved/static/js/runtime-main.6d1a57d1.js"
+  },
+  {
+    "revision": "26ef8bb418031b9bb4f44e1aeea71186",
+    "url": "/pokedex-improved/static/media/spinner.26ef8bb4.gif"
+  }
+]);
